20586a4bc7686cc9dc49da3f391318147fcef073
nazzy-tech <58662791+nazzy-tech@users.noreply.github.com>
2024-03-07 11:19:49 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+// Hamburger Menu
+let dropDown = document.getElementById("dropDownList");
+let menu = document.getElementById("btnMenu");
+let closebtn = document.getElementById("bxClose");
+
+bntOpen = false;
+
+menu.addEventListener('click', function() {
+
+    if (bntOpen == false) {
+        dropDownList.style.display = 'block'
+
+        bntOpen = true
+    }
+    closebtn.addEventListener('click', () => {
+        if (bntOpen == true) {
+            dropDownList.style.display = 'none'
+            bntOpen = false
+        }
+    })
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Set the date we're counting down to
 var countDownDate = new Date("Mar 30, 2024 24:00:00").getTime();
 
